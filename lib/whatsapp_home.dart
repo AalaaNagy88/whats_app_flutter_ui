5e7db13d66013529752c569d037605badd4ee11d
alaa <salaanagy4444@gmail.com>
2019-11-15 05:58:55 +0200

    second commit

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -7,18 +7,31 @@ class whatsAppHome extends StatefulWidget {
   }
 }
 
-}
 class _whatsAppHomeState extends State<whatsAppHome> with SingleTickerProviderStateMixin{
   TabController _tabController;
-  
+  @override
+  void initState() {
+    super.initState();
+    _tabController =new TabController(vsync: this,initialIndex: 1,length: 4);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("WhatsApp"),
         elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt),),
+            Tab(text: "Chats",),
+            Tab(text: "Status",),
+            Tab(text: "Calls",)
+          ],
+        ),
       ),
+      body: Container(),
     );
   }
-
 }
